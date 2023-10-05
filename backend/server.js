64003122f56e9dd1d70a1395f003acc58a93ca6f
Kundan Prasad <kp800532@gmail.com>
2023-10-06 01:43:01 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import templateRoutes from './routes/templateRoutes.js';
-import mailRoutes from './routes/mailRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import connectDatabase from './config/database.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import {path} from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import templateRoutes from "./routes/templateRoutes.js";
+import mailRoutes from "./routes/mailRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDatabase from "./config/database.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -16,32 +16,34 @@ connectDatabase();
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors() )
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use('/api/users',userRoutes);
+app.use("/api/users", userRoutes);
 
-app.use('/api/mail',templateRoutes);
+app.use("/api/mail", templateRoutes);
 
-app.use('/api/',mailRoutes);
+app.use("/api/", mailRoutes);
 
-if(process.env.NODE_ENV === 'production'){
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'..frontend','dist','index.html')));
-}else{
-    app.get('/',(req,res)=>{
-        res.send('Server is ready');
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server is ready");
+  });
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
