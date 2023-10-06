b83634df4335fb015b48d48347a9128f04751c41
Kundan Prasad <kp800532@gmail.com>
2023-10-06 21:14:52 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -28,18 +28,10 @@ app.use("/api/mail", templateRoutes);
 
 app.use("/api/", mailRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-  });
-} else {
   app.get("/", (req, res) => {
     res.send("Server is ready");
   });
-}
 
 app.use(notFound);
 app.use(errorHandler);
