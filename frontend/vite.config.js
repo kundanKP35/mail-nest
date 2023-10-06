89afb39cc2bf7a65c7d797a74f7d562f49a1c016
Kundan Prasad <kp800532@gmail.com>
2023-10-06 21:40:45 +0530

    pre deploy vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
 
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
