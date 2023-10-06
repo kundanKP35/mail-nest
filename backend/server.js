314eb2beb52d8f1bd0cb8dccc07ee02040fbe5d5
Kundan Prasad <kp800532@gmail.com>
2023-10-06 22:44:52 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ connectDatabase();
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+
+  credentials : true,
+  origin : "https://mail-nest.vercel.app"
+}
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
